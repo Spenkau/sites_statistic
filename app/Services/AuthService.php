b94ba30b64d796d28e615362ba732087f03764d9
Aliaksandr Khaliauski <kholyavskij123@gmail.com>
2024-03-04 16:54:03 +0300

    add auth service and implement auth logic in repository

---
+++ app/Services/AuthService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\AuthRepository;
+use Exception;
+
+class AuthService
+{
+    protected AuthRepository $authRepository;
+
+    public function __construct(AuthRepository $authRepository)
+    {
+        $this->authRepository = $authRepository;
+    }
+
+    public function login(array $data): array
+    {
+        return $this->authRepository->login($data);
+    }
+
+    public function register(array $data): array
+    {
+        return $this->authRepository->register($data);
+    }
+}
