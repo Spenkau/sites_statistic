413d41b2c3742ecb84456c04b6857be4d4b526d8
Aliaksandr Khaliauski <kholyavskij123@gmail.com>
2024-03-04 11:17:37 +0300

    create controllers for auth and test

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        
+    }
+}
