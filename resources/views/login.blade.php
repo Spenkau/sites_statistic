e4efd3d2a7966c1f6560ba33d0eb0ec1ce2cf19d
Aliaksandr Khaliauski <kholyavskij123@gmail.com>
2024-03-04 16:52:22 +0300

    add blades for auth and layouts

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,15 +1,29 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-    <title>Laravel</title>
-</head>
-<body class="antialiased">
-<nav class="d-flex nav">
-    <a href="{{ route('login') }}">Вход</a>
-    <a href="{{ route('register') }}">Регистрация</a>
-</nav>
-</body>
-</html>
+@section('content')
+    <div>
+        <h1>{{ __('Login') }}</h1>
+        <form method="POST" action="/api/login" style="text-align: center; margin: auto;">
+            @csrf
+            @method('POST')
+            <div>
+                <label for="email">Email</label>
+                <input class="is-invalid" type="email" name="email" id="email" value="{{ old('email') }}">
+            </div>
+
+            <div>
+
+                <label for="password">Пароль</label>
+                <input type="password" name="password" id="password">
+            </div>
+
+            <div>
+                <label for="remember_me">Запомнить меня</label>
+                <input type="checkbox" name="remember_me" id="remember_me">
+            </div>
+
+            <button type="submit">Вход</button>
+        </form>
+    </div>
+
+@endsection
