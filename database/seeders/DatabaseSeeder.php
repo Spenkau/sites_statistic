0d72477324cf299d12f941e043f19e9e974fac1f
Aliaksandr Khaliauski <kholyavskij123@gmail.com>
2024-03-05 14:08:14 +0300

    update factory and enums

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\Page;
+use App\Models\Detail;
+use App\Models\Site;
 use Database\Factories\PageFactory;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -11,6 +12,8 @@ use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
+    protected array $responseCodes = [200, 300, 400, 500];
+
     /**
      * Seed the application's database.
      */
@@ -18,7 +21,15 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        Page::factory(30)->create();
+        Detail::factory()->create([
+            'page_id' => 1,
+            'status_code' => $this->responseCodes[rand(0, count($this->responseCodes) - 1)],
+            'response_time' => rand(100, 10000),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        Site::factory(30)->create();
 
          \App\Models\User::factory()->create([
              'name' => 'test',
