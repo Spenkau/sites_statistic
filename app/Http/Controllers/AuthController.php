19fac827c21c09af36496d0209b6668308bb094e
Aliaksandr Khaliauski <kholyavskij123@gmail.com>
2024-03-06 17:35:04 +0300

    auth refactor

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,42 +4,124 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\User;
 use App\Services\AuthService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    public AuthService $authService;
 
-    public function __construct(AuthService $authService)
+    public function register(Request $request)
     {
-        $this->authService = $authService;
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'c_password' => 'required|same:password',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] =  $user->createToken('MyApp')->accessToken;
+        $success['name'] =  $user->name;
+
+        return $this->sendResponse($success, 'User register successfully.');
     }
 
-    public function register(RegisterRequest $request)
+    public function login(Request $request)
     {
-        $registerData = $request->validated();
-
-        $response = $this->authService->register($registerData);
-
-        return response()->json($response, 200);
-    }
-
-    public function login(LoginRequest $request)
-    {
-        $loginData = $request->validated();
-
-        $response = $this->authService->login($loginData);
-
-        return response()->json($response);
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = Auth::user();
+            $success['token'] =  $user->createToken('MyApp')->accessToken;
+            $success['name'] =  $user->name;
+            return $this->sendResponse($success, 'User login successfully.');
+        } else {
+            return $this->sendError('Unauthorised.', ['error' => 'Unauthorised']);
+        }
     }
 
     public function logout(Request $request)
     {
-        $request->user()->token()->revoke();
+        if (Auth::user()) {
+            $user = Auth::user()->token();
+            $user->revoke();
 
-        return response([
-            'message' => 'Logged out successfully'
-        ]);
+            return response()->json([
+                'success' => true,
+                'message' => 'Logout successfully'
+            ]);
+        }else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Unable to Logout'
+            ]);
+        }
     }
+
+    public function sendResponse($result, $message)
+    {
+        $response = [
+            'success' => true,
+            'data'    => $result,
+            'message' => $message,
+        ];
+        return response()->json($response, 200);
+    }
+
+    public function sendError($error, $errorMessages = [], $code = 404)
+    {
+        $response = [
+            'success' => false,
+            'message' => $error,
+        ];
+
+        if (!empty($errorMessages)) {
+            $response['data'] = $errorMessages;
+        }
+
+        return response()->json($response, $code);
+    }
+
+
+
+//    public AuthService $authService;
+//
+//    public function __construct(AuthService $authService)
+//    {
+//        $this->authService = $authService;
+//    }
+//
+//    public function register(RegisterRequest $request)
+//    {
+//        $registerData = $request->validated();
+//
+//        $response = $this->authService->register($registerData);
+//
+//        return redirect()->to('/')->with($response);
+//    }
+//
+//    public function login(LoginRequest $request)
+//    {
+//        $loginData = $request->validated();
+//
+//        $response = $this->authService->login($loginData);
+//
+//        return redirect()->to('/')->with($response);
+//    }
+//
+//    public function logout(Request $request)
+//    {
+//        $request->user()->token()->revoke();
+//
+//        return response([
+//            'message' => 'Logged out successfully'
+//        ]);
+//    }
 }
