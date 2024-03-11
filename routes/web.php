371c5b76a8d45d05d238b53d965db0e670f16aba
Aliaksandr Khaliauski <kholyavskij123@gmail.com>
2024-03-11 18:23:50 +0300

    add resource route for site and page, add route 'test' for testing

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SiteController;
+use App\Http\Middleware\CheckSiteOwner;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Validator;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,15 +23,52 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('test', function () {
+
+    $start = microtime(true);
+    try {
+        $response = Http::get('https://vpodarok.ru/');
+
+        $data = [
+            'page_id' => 2,
+            'status_code' => $response->status(),
+            'response_time' => number_format(microtime(true) - $start, 3, '.', '')
+        ];
+
+        $validator = Validator::make($data, [
+            'page_id' => 'required|integer',
+            'status_code' => 'required|integer',
+            'response_time' => 'required|numeric'
+        ]);
+
+        $detail = $validator->validated();
+
+        return $detail;
+    } catch (Exception $exception) {
+        return [$exception->getCode(), $exception->getMessage(), $exception->getTraceAsString()];
+    }
+
+});
+
 Route::get('/dashboard', [SiteController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::resource('site', SiteController::class);
-    Route::resource('page', PageController::class);
+
+    Route::middleware('site.owner')->group(function () {
+        Route::resource('site', SiteController::class);
+        Route::resource('site.page', PageController::class)->middleware('site.id');
+    });
+
+    Route::resource('profile', ProfileController::class)->only(['update', 'destroy']);
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::fallback(function () {
+        return redirect()->route('dashboard');
+    });
+
 });
 
 require __DIR__.'/auth.php';
