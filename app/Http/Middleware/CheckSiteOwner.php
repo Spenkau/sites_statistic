1b163d0a1ec1b7797a5367440bd8c647befe57d8
Aliaksandr Khaliauski <kholyavskij123@gmail.com>
2024-03-11 18:21:53 +0300

    add middleware to protect user sites and pages

---
+++ app/Http/Middleware/CheckSiteOwner.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Page;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckSiteOwner
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $site = $request->route('site');
+
+        if ($site && $site->user->id != Auth::id()) {
+            return redirect('dashboard')->with('error', 'Unauthorized action.');
+        }
+
+        return $next($request);
+
+    }
+}
