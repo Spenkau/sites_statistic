933db8887ce394733914f53e485ac0b664ea6d8f
Aliaksandr Khaliauski <kholyavskij123@gmail.com>
2024-03-13 13:53:32 +0300

    add example of test file

---
+++ tests/Repositories/SiteRepositoryTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Repositories;
+
+use App\Repositories\SiteRepository;
+use PHPUnit\Framework\TestCase;
+
+class SiteRepositoryTest extends TestCase
+{
+
+    public function testGetUserId()
+    {
+
+    }
+
+    public function testStore()
+    {
+
+    }
+
+    public function testFindById()
+    {
+
+    }
+
+    public function testAll()
+    {
+
+    }
+
+    public function testAddCollaborator()
+    {
+
+    }
+
+    public function test__construct()
+    {
+
+    }
+
+    public function testUpdate()
+    {
+
+    }
+
+    public function testDestroy()
+    {
+
+    }
+}
