fded144f2c0c93adfd2401df0ffc9d5a50f6b94b
Aliaksandr Khaliauski <kholyavskij123@gmail.com>
2024-03-18 22:58:47 +0300

    create UpdateApiPointDetails.php command

---
+++ app/Console/Commands/UpdateApiPointDetailsCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class UpdateApiPointDetailsCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'update:api-point-details';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update details of api point';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
