98bb23e176167f88d00c6b693e339b3d7b3eb517
Aliaksandr Khaliauski <kholyavskij123@gmail.com>
2024-03-18 23:01:16 +0300

    create test version of job UpdateApiPointDetails.php

--- app/Jobs/UpdateApiPointDetails.php
+++ app/Jobs/UpdateApiPointDetails.php
@@ -2,22 +2,47 @@
 
 namespace App\Jobs;
 
+use App\Enums\ApiServiceEnum;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Http;
 
 class UpdateApiPointDetails implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    private array $services;
+
+    private array $headers;
+
+    private string $url = 'https://preprod-vpdrk.hellishworld.ru/';
     /**
      * Create a new job instance.
      */
     public function __construct()
     {
-        //
+        $this->services = Config::get('api_services')['services'];
+
+        $this->authenticateSession();
+    }
+
+    public function authenticateSession(): void
+    {
+        $response = Http::get('login', [
+            'email' => 'danyat@test.ru',
+            'password' => 'test'
+        ]);
+
+        $bearerToken = $response['token'];
+
+        $this->headers = [
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer ' . $bearerToken
+        ];
     }
 
     /**
@@ -25,6 +50,18 @@ class UpdateApiPointDetails implements ShouldQueue
      */
     public function handle(): void
     {
-        //
+        $serviceNames = array_column(ApiServiceEnum::cases(), 'value');
+
+        foreach ($serviceNames as $serviceName) {
+            $service = $this->services[$serviceName];
+
+            $method = $service['method'];
+            $headers = $this->headers;
+            $url = $this->url . $serviceName;
+            $params = $service['parameters'];
+
+            Http::withHeaders($headers)
+                ->{$method}($url, $params);
+        }
     }
 }
