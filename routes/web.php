19891c0982540a540d976ce42c822a79cbb24992
Aliaksandr Khaliauski <kholyavskij123@gmail.com>
2024-03-19 18:17:05 +0300

    add logic for testing v2 api

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,8 @@ use App\Http\Controllers\PageController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SiteController;
 use App\Http\Controllers\UserController;
-use App\Models\Page;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -55,7 +53,7 @@ Route::middleware('auth')->group(function () {
             Route::get('/', 'edit')->name('edit');
             Route::patch('/', 'update')->name('update');
             Route::delete('/', 'destroy')->name('destroy');
-    });
+        });
 
     Route::fallback(function () {
         return redirect()->route('dashboard');
@@ -63,9 +61,53 @@ Route::middleware('auth')->group(function () {
 
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
 
 Route::get('test', function () {
+    $url = 'https://preprod-vpdrk.hellishworld.ru/api/v2/';
 
+    $token = Http::get($url . 'login?email=danyat@test.ru&password=test')['token'];
+
+    $headers = [
+        'Accept' => 'application/json',
+        'Authorization' => 'Bearer ' . $token
+    ];
+
+    $serviceNames = array_column(ApiServiceEnum::cases(), 'value');
+    $services = Config::get('api_services');
+
+    $responses = [];
+
+    foreach ($serviceNames as $serviceName) {
+        try {
+            $service = $services[$serviceName];
+
+            $method = $service['method'] ?? 'GET';
+            $newUrl = $url . $serviceName;
+            $params = $service['parameters'] ?? [];
+
+            $responseTemplate = "Http::withHeaders(" . json_encode($headers) . ")->send('" . $method . "','" . $newUrl . "'," . json_encode($params) . ");";
+
+            if ($method == 'GET') {
+                $response = Http::withHeaders($headers)
+                    ->get($newUrl, $params);
+            } else if ($method == 'POST') {
+                $response = Http::withHeaders($headers)
+                    ->post($newUrl, $params);
+            }
+
+            $responses[] = [
+                'res' => $response->successful() ? json_decode($response->body()) : false,
+                'error' => $response->failed() ? $response->body() : false,
+                'template' => $responseTemplate,
+                'params' => $params
+            ];
+        } catch (Exception $exception) {
+            continue;
+        }
+    }
+
+//    return Config::get('auth_api_services');
+    return $responses;
 });
